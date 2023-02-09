813e8a7acb6cef7c9692bda901d83ba62e13c52d
Chuca <Chuca78@users.noreply.github.com>
2023-02-09 12:30:31 -0500

    built contact form (frontend)

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -3,6 +3,8 @@ import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 // import ContactForm from '../components/contact-form';
 import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 // import "../App.css";
@@ -13,7 +15,7 @@ export default function Contact() {
       <Jumbotron fluid className="p-5 mb-0">
         <Container className="text-center">
           <div className="p-md-5">
-            <h1>Get in Touch</h1>
+            <h1>Contact Me</h1>
           </div>
         </Container>
       </Jumbotron>
@@ -23,7 +25,7 @@ export default function Contact() {
           <Row>
             <Col>
               <Container>
-                <h4 style={{ color: "#F06543" }}>Contact Info</h4>
+                <h4 style={{ color: "#D6CCC2" }}>Contact Info</h4>
                 <p>
                   <a href="mailto:tfleck78@gmail.com">
                     <span className="pr-3">
@@ -57,8 +59,53 @@ export default function Contact() {
 
             <Col sm={12} md={6} lg={8} className="pt-sm-4 pt-lg-0">
               <Container>
-                <h4 style={{ color: "#F06543" }}>Send a Message</h4>
-                {/* <ContactForm /> */}
+                <h4 style={{ color: "#D6CCC2" }}>Send a Message</h4>
+                <Form id="contactForm">
+                  <Form.Group controlId="name">
+                    <Form.Label>Your Name</Form.Label>
+                    <Form.Control
+                      required
+                      name="name"
+                      placeholder="Your Name"
+                    />
+                  </Form.Group>
+
+                  <Form.Group controlId="email">
+                    <Form.Label>Your Email</Form.Label>
+                    <Form.Control
+                      required
+                      name="email"
+                      type="email"
+                      placeholder="name@email.com"
+                    />
+                  </Form.Group>
+
+                  <Form.Group controlId="subject">
+                    <Form.Label>Subject</Form.Label>
+                    <Form.Control
+                      required
+                      name="subject"
+                      placeholder="Subject"
+                    />
+                  </Form.Group>
+
+                  <Form.Group controlId="message">
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control
+                      required
+                      name="message"
+                      as="textarea"
+                      rows="5"
+                      placeholder="Message"
+                    />
+                  </Form.Group>
+
+                  {<p className="form-message"></p>}
+
+                  <Button type="submit" className="btn">
+                    Submit
+                  </Button>
+                </Form>
               </Container>
             </Col>
           </Row>
