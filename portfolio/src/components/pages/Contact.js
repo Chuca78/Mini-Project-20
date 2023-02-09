7422ba4161345206be948e42933c93eb34d03e4c
Chuca <Chuca78@users.noreply.github.com>
2023-02-09 11:58:29 -0500

    updated page names

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -1,22 +1,69 @@
-import React from 'react';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+// import ContactForm from '../components/contact-form';
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+// import "../App.css";
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+    <div className="container">
+      <Jumbotron fluid className="p-5 mb-0">
+        <Container className="text-center">
+          <div className="p-md-5">
+            <h1>Get in Touch</h1>
+          </div>
+        </Container>
+      </Jumbotron>
+
+      <Container fluid className="shadow bg-light mb-5">
+        <Container className="p-4">
+          <Row>
+            <Col>
+              <Container>
+                <h4 style={{ color: "#F06543" }}>Contact Info</h4>
+                <p>
+                  <a href="mailto:tfleck78@gmail.com">
+                    <span className="pr-3">
+                      <FaEnvelope />
+                    </span>
+                    tfleck78@gmail.com
+                  </a>
+                </p>
+
+                <p>
+                  <a href="https://github.com/Chuca78" target="_blank">
+                    <span className="pr-3">
+                      <FaGithub />
+                    </span>
+                    Chuca78
+                  </a>
+                </p>
+                <p>
+                  <a
+                    href="https://www.linkedin.com/in/timothyfleck/"
+                    target="_blank"
+                  >
+                    <span className="pr-3">
+                      <FaLinkedin />
+                    </span>
+                    timothy-fleck
+                  </a>
+                </p>
+              </Container>
+            </Col>
+
+            <Col sm={12} md={6} lg={8} className="pt-sm-4 pt-lg-0">
+              <Container>
+                <h4 style={{ color: "#F06543" }}>Send a Message</h4>
+                {/* <ContactForm /> */}
+              </Container>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
     </div>
   );
 }
