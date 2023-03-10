d3c5dcfae76418bcab4b3c5c90dd6a0e17ae9e08
Chuca <Chuca78@users.noreply.github.com>
2023-03-10 06:55:22 -0500

    Updates

---
+++ precache-manifest.2eb3bcadd4584b9caa0f25d5ee5b96e1.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa69fc3369c2ff9350ed",
+    "url": "/React-Portfolio/static/css/main.27fbe63e.chunk.css"
+  },
+  {
+    "revision": "fa69fc3369c2ff9350ed",
+    "url": "/React-Portfolio/static/js/main.1e625d72.chunk.js"
+  },
+  {
+    "revision": "a5982163e5c3e3252977",
+    "url": "/React-Portfolio/static/js/runtime~main.982e8b01.js"
+  },
+  {
+    "revision": "581a386d4f7f3536c29f",
+    "url": "/React-Portfolio/static/css/2.8425e95f.chunk.css"
+  },
+  {
+    "revision": "581a386d4f7f3536c29f",
+    "url": "/React-Portfolio/static/js/2.f3567b4f.chunk.js"
+  },
+  {
+    "revision": "dc28ac1424ccf7c65fc16f1ef7905bc7",
+    "url": "/React-Portfolio/static/media/Background2.dc28ac14.png"
+  },
+  {
+    "revision": "c34dfff6b89308cdaa88fa20eabb6885",
+    "url": "/React-Portfolio/static/media/UT_Austin.c34dfff6.png"
+  },
+  {
+    "revision": "ae450bebbf7ce5d818d0548d0401ba0b",
+    "url": "/React-Portfolio/static/media/ERAU.ae450beb.png"
+  },
+  {
+    "revision": "bf208f839fc2d1d4a9aaf9b275d57c27",
+    "url": "/React-Portfolio/static/media/Kirkwood.bf208f83.jpg"
+  },
+  {
+    "revision": "46b241e1b8dc4dc07749bf8bf6169556",
+    "url": "/React-Portfolio/static/media/Food-App.46b241e1.png"
+  },
+  {
+    "revision": "a76dc9b8f890cc2be08f4487df6f584e",
+    "url": "/React-Portfolio/static/media/weather_application.a76dc9b8.png"
+  },
+  {
+    "revision": "61dfb11eda7ac44222e4c04edd4fb9ff",
+    "url": "/React-Portfolio/static/media/Tech-Blog.61dfb11e.png"
+  },
+  {
+    "revision": "037d2611b026104ffc19ff05af23f172",
+    "url": "/React-Portfolio/static/media/Code-Quiz.037d2611.png"
+  },
+  {
+    "revision": "d74cde6ad578b497d809ddcab6e8106e",
+    "url": "/React-Portfolio/static/media/JATE.d74cde6a.png"
+  },
+  {
+    "revision": "7826fe1a811d7bdc09707cf9eb72dd53",
+    "url": "/React-Portfolio/static/media/Regex_tutorial.7826fe1a.png"
+  },
+  {
+    "revision": "922b66b3ea105999e218f94d7f3dfb6f",
+    "url": "/React-Portfolio/static/media/Work-Day-Scheduler.922b66b3.png"
+  },
+  {
+    "revision": "6cde666dd95b5cf53ad42bff1729922d",
+    "url": "/React-Portfolio/static/media/Note-taker.6cde666d.png"
+  },
+  {
+    "revision": "a3b71d5222293ba7face721cc45f5c3b",
+    "url": "/React-Portfolio/static/media/Random-Password-Generator.a3b71d52.png"
+  },
+  {
+    "revision": "9658fa83703aa8dce15b30eaff24840c",
+    "url": "/React-Portfolio/static/media/READme_Generator.9658fa83.png"
+  },
+  {
+    "revision": "48fcdc3e1e11a9f134766c00dfec029e",
+    "url": "/React-Portfolio/static/media/MERN.48fcdc3e.png"
+  },
+  {
+    "revision": "86695d5d2f75c08615e5db9c648e099f",
+    "url": "/React-Portfolio/static/media/logo.86695d5d.png"
+  },
+  {
+    "revision": "379fe880d7638d8d5e6493d9dcfac94c",
+    "url": "/React-Portfolio/static/media/jquery-logo.379fe880.png"
+  },
+  {
+    "revision": "4bb62f3c2a6c2b09fc75d23eaa74fcfb",
+    "url": "/React-Portfolio/static/media/bootstrap-logo.4bb62f3c.png"
+  },
+  {
+    "revision": "8967ca67bbedb067ba0aedd74a407295",
+    "url": "/React-Portfolio/static/media/mern-logo.8967ca67.png"
+  },
+  {
+    "revision": "f7099627bd7c464de1c0b40abf87ad86",
+    "url": "/React-Portfolio/static/media/webpack-logo.f7099627.png"
+  },
+  {
+    "revision": "21812f6d8d4844da17453b80216a4445",
+    "url": "/React-Portfolio/static/media/sql-logo.21812f6d.png"
+  },
+  {
+    "revision": "541dcec40560667c49cccd764392f3d4",
+    "url": "/React-Portfolio/static/media/mongodb-logo.541dcec4.png"
+  },
+  {
+    "revision": "9b88e932a50e847601ee40ca4588fc13",
+    "url": "/React-Portfolio/static/media/React-logo.9b88e932.png"
+  },
+  {
+    "revision": "e68e09e9990d741086e337d59744fbb4",
+    "url": "/React-Portfolio/index.html"
+  }
+];
